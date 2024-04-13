e7d30eac4d7282c11b7e9928c90ca473beb2af47
Przemekpia <przemekpia@op.pl>
2024-04-14 00:01:45 +0200

    input table + color changed

--- src/App.js
+++ src/App.js
@@ -10,89 +10,143 @@ function App() {
         height: "100vh",
         width: "100vw",
         color: "white",
-        display: "flex",
-        alignItems: "center",
-        justifyContent: "center",
       }}
     >
-      <table>
-        <thead>
-          <tr>
-            <th></th>
-            <th>Włócznik</th>
-            <th>Łucznik</th>
-            <th>Jeździec</th>
-            <th>Gryf</th>
-            <th>Rycerz</th>
-            <th>Potwory</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr style={{backgroundColor: "white", color: "black"}}>
-            <th>I</th>
-            <th>0</th>
-            <th>0</th>
-            <th>0</th>
-            <th>0</th>
-            <th>0</th>
-            <th>0</th>
-          </tr>
-          <tr style={{backgroundColor: "green", color: "black"}}>
-            <th>II</th>
-            <th>0</th>
-            <th>0</th>
-            <th>0</th>
-            <th>0</th>
-            <th>0</th>
-            <th>0</th>
-          </tr>
-          <tr style={{backgroundColor: "blue", color: "black"}}>
-            <th>III</th>
-            <th>0</th>
-            <th>0</th>
-            <th>0</th>
-            <th>0</th>
-            <th>0</th>
-            <th>0</th>
-          </tr>
-          <tr style={{backgroundColor: "purple", color: "black"}}>
-            <th>IV</th>
-            <th>0</th>
-            <th>0</th>
-            <th>0</th>
-            <th>0</th>
-            <th>0</th>
-            <th>0</th>
-          </tr>
-          <tr style={{backgroundColor: "orange", color: "black"}}>
-            <th>V</th>
-            <th>0</th>
-            <th>0</th>
-            <th>0</th>
-            <th>0</th>
-            <th>0</th>
-            <th>0</th>
-          </tr>
-          <tr style={{backgroundColor: "red", color: "black"}}>
-            <th>VI</th>
-            <th>0</th>
-            <th>0</th>
-            <th>0</th>
-            <th>0</th>
-            <th>0</th>
-            <th>0</th>
-          </tr>
-          <tr style={{backgroundColor: "yellow", color: "black"}}>
-            <th>VII</th>
-            <th>0</th>
-            <th>0</th>
-            <th>0</th>
-            <th>0</th>
-            <th>0</th>
-            <th>0</th>
-          </tr>
-        </tbody>
-      </table>
+      <div
+        style={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          paddingTop:"15vh"
+        }}
+      >
+        <table>
+          <tbody>
+            <tr>
+              <th>Zakres gwardzistów:</th>
+              <th>
+                <input></input>
+              </th>
+              <th>
+                <input></input>
+              </th>
+              <th>Bonus gwardzistów:</th>
+              <th>
+                <input></input>
+              </th>
+            </tr>
+            <tr>
+              <th>Zakres rycerzy:</th>
+              <th>
+                <input></input>
+              </th>
+              <th>
+                <input></input>
+              </th>
+              <th>Bonus rycerzy:</th>
+              <th>
+                <input></input>
+              </th>
+            </tr>
+            <tr>
+              <th>Pojemnosc:</th>
+              <th>
+                <input></input>
+              </th>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+
+      <div>
+        <button>Oblicz</button>
+      </div>
+      <div
+        style={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        <table>
+          <thead>
+            <tr>
+              <th></th>
+              <th>Włócznik</th>
+              <th>Łucznik</th>
+              <th>Jeździec</th>
+              <th>Gryf</th>
+              <th>Rycerz</th>
+              <th>Najemnicy</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr style={{ backgroundColor: "#d6cbb7", color: "black" }}>
+              <th>I</th>
+              <th>0</th>
+              <th>0</th>
+              <th>0</th>
+              <th>0</th>
+              <th>0</th>
+              <th>0</th>
+            </tr>
+            <tr style={{ backgroundColor: "#416b00", color: "black" }}>
+              <th>II</th>
+              <th>0</th>
+              <th>0</th>
+              <th>0</th>
+              <th>0</th>
+              <th>0</th>
+              <th>0</th>
+            </tr>
+            <tr style={{ backgroundColor: "#005b8f", color: "black" }}>
+              <th>III</th>
+              <th>0</th>
+              <th>0</th>
+              <th>0</th>
+              <th>0</th>
+              <th>0</th>
+              <th>0</th>
+            </tr>
+            <tr style={{ backgroundColor: "#470b85", color: "black" }}>
+              <th>IV</th>
+              <th>0</th>
+              <th>0</th>
+              <th>0</th>
+              <th>0</th>
+              <th>0</th>
+              <th>0</th>
+            </tr>
+            <tr style={{ backgroundColor: "#a43700", color: "black" }}>
+              <th>V</th>
+              <th>0</th>
+              <th>0</th>
+              <th>0</th>
+              <th>0</th>
+              <th>0</th>
+              <th>0</th>
+            </tr>
+            <tr style={{ backgroundColor: "#d20c0b", color: "black" }}>
+              <th>VI</th>
+              <th>0</th>
+              <th>0</th>
+              <th>0</th>
+              <th>0</th>
+              <th>0</th>
+              <th>0</th>
+            </tr>
+            <tr style={{ backgroundColor: "#cb8700", color: "black" }}>
+              <th>VII</th>
+              <th>0</th>
+              <th>0</th>
+              <th>0</th>
+              <th>0</th>
+              <th>0</th>
+              <th>0</th>
+            </tr>
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 }
