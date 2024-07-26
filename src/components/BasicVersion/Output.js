859ffcbfffecdab8a41164cdd72e43d26f3df721
przemekpia <przemekpia@gmail.com>
2024-07-26 17:57:22 +0200

    FIrst version of army calculation

--- src/components/BasicVersion/Output.js
+++ src/components/BasicVersion/Output.js
@@ -6,30 +6,54 @@ const Output = () => {
     color: "black",
     padding: "10px 20px",
     cursor: "pointer",
-    display: "block", 
+    display: "block",
     borderRadius: "5px",
     textAlign: "center",
     minWidth: "100px",
     backgroundColor: "white",
   };
 
-  
   const selectedUnits = useSelector((state) => state.army.selectedUnits || []);
-  const selectedMonsters = useSelector((state) => state.army.selectedMonsters || []);
-  const leadership = 10000
-  
+  const selectedMonsters = useSelector(
+    (state) => state.army.selectedMonsters || []
+  );
+  const leadership = 10000;
+
   const handleClick = () => {
-    const sortedUnits = selectedUnits
-      .map(unit => ({
+    let sortedUnits = selectedUnits
+      .map((unit) => ({
         ...unit,
-        ratio: unit.attack / unit.leadership
+        ratio: unit.attack / unit.leadership,
+        amount: 0,
       }))
       .sort((a, b) => b.ratio - a.ratio);
 
+    let currentLeadership = 0;
+    while (currentLeadership <= leadership) {
+      for (let j = 0; j < sortedUnits.length; j++) {
+        if (j + 1 === sortedUnits.length) {
+          sortedUnits[j].amount += 1;
+          currentLeadership += sortedUnits[j].leadership;
+        } else {
+          if (
+            (sortedUnits[j].amount + 1) * sortedUnits[j].hp <
+              sortedUnits[j + 1].amount * sortedUnits[j + 1].hp &&
+            currentLeadership + sortedUnits[j].leadership <= leadership
+          ) {
+            sortedUnits[j].amount += 1;
+            currentLeadership += sortedUnits[j].leadership;
+            break;
+          }
+        }
+      }
+    }
 
-    console.log("Sorted Units:", sortedUnits);
-
-    alert("Sorted Units: " + sortedUnits.map(unit => `${unit.name}: ${unit.ratio.toFixed(2)}`).join(", "));
+    alert(
+      "Sorted Units: " +
+        sortedUnits
+          .map((unit) => `${unit.name}: ${unit.amount}`)
+          .join(", ")
+    );
   };
 
   return (
